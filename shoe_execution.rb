e7e802d606454553a7f2aefd35bbd71cb6813d2e
lp <lp@spiralix.org>
2009-01-02 13:48:56 -0500

    integrated all socks.rb execution inside shoe_execution.rb helper

--- shoe_execution.rb
+++ shoe_execution.rb
@@ -1,17 +1,56 @@
 module ShoeHelpers
+  require 'hashfs'
+  require 'ftools'
   
   def execution
     layout do
       @progress = progress :width => 300
     end
     @title.replace 'Backuping:'
-    @sock = Socks.new(self, @loader, {:progress => @progress, :display => @display})
     @display.replace "scanning your backup forder"
-    @sock.lace
-    @sock.timeFreeze(@loader.extension)
-    @sock.walk
-    @sock.saveData
-    @sock.printDebug('IOOOI')
+    lace; timeFreeze; walk; saveData
+  end
+  
+  private
+  
+  def lace
+    @srcfs = Hashfs.new(@loader.source)
+    @srcfs.scan
+    @display.replace "looking for files to backup"
+    if @loader.type == 'local'
+      makedirs(@loader.destination) unless File.exist?(@loader.destination)
+      @destfs = Hashfs.load(@loader.destination)
+    else
+      @smb = SmbHelper.new(:host => @loader.host, :share => @loader.share, :user => @loader.user, :password => @loader.password)
+      @destfs = @smb.get_destfs(@loader.source)
+    end
+    @trail = Hashfs.compare(@srcfs, @destfs)
+  end
+  
+  def timeFreeze
+    unless @loader.extension == 'none'
+      h = SafeHistory.new(@loader.extension,@srcfs.root,@srcfs.fs)
+      h.go
+    end
+    Hashfs.historyToSafety(h,@trail)
+  end
+  
+  def walk
+    anim = animate do |i|
+      @progress.fraction = @trail.progress
+      @display.replace "backuping file: #{@trail.current_file} progress= #{@trail.progress}"
+      @trail.step
+      anim.stop if @trail.complete
+    end
+  end
+  
+  def saveData
+    @destfs.merge(@srcfs)
+    @destfs.dump
+  end
+  
+  def printDebug(text)
+    Hashfs.printDebug(text)
   end
   
 end
