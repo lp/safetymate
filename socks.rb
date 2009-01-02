a0085c57c4717306d2e7436d62f60929aa94e6ff
lp <lp@spiralix.org>
2009-01-02 13:50:07 -0500

    commented all lines before removal (???)

--- socks.rb
+++ socks.rb
@@ -8,53 +8,53 @@
 # :title:Socks
 
 class Socks
-  require 'hashfs'
-  require 'ftools'
+  # require 'hashfs'
+  # require 'ftools'
   
-  def initialize(shoes, loader, controls)
-    @shoes = shoes
-    @loader = loader
-    @controls = controls
-  end
+  # def initialize(shoes, loader, controls)
+  #   @shoes = shoes
+  #   @loader = loader
+  #   @controls = controls
+  # end
 
-  def lace
-    @srcfs = Hashfs.new(@loader.source)
-    @srcfs.scan
-    @controls[:display].replace "looking for files to backup"
-    if @loader.type == 'local'
-      makedirs(@loader.destination) unless File.exist?(@loader.destination)
-      @destfs = Hashfs.load(@loader.destination)
-    else
-      @smb = SmbHelper.new(:host => @loader.host, :share => @loader.share, :user => @loader.user, :password => @loader.password)
-      @destfs = @smb.get_destfs(@loader.source)
-    end
-    @trail = Hashfs.compare(@srcfs, @destfs)
-  end
+  # def lace
+  #   @srcfs = Hashfs.new(@loader.source)
+  #   @srcfs.scan
+  #   @controls[:display].replace "looking for files to backup"
+  #   if @loader.type == 'local'
+  #     makedirs(@loader.destination) unless File.exist?(@loader.destination)
+  #     @destfs = Hashfs.load(@loader.destination)
+  #   else
+  #     @smb = SmbHelper.new(:host => @loader.host, :share => @loader.share, :user => @loader.user, :password => @loader.password)
+  #     @destfs = @smb.get_destfs(@loader.source)
+  #   end
+  #   @trail = Hashfs.compare(@srcfs, @destfs)
+  # end
   
-  def timeFreeze(ext)
-    unless ext == 'none'
-      h = SafeHistory.new(ext,@srcfs.root,@srcfs.fs)
-      h.go
-    end
-    Hashfs.historyToSafety(h,@trail)
-  end
+  # def timeFreeze(ext)
+  #   unless ext == 'none'
+  #     h = SafeHistory.new(ext,@srcfs.root,@srcfs.fs)
+  #     h.go
+  #   end
+  #   Hashfs.historyToSafety(h,@trail)
+  # end
   
-  def walk
-    anim = @shoes.animate do |i|
-      @controls[:progress].fraction = @trail.progress
-      @controls[:display].replace "backuping file: #{@trail.current_file} progress= #{@trail.progress}"
-      @trail.step
-      anim.stop if @trail.complete
-    end
-  end
+  # def walk
+  #   anim = @shoes.animate do |i|
+  #     @controls[:progress].fraction = @trail.progress
+  #     @controls[:display].replace "backuping file: #{@trail.current_file} progress= #{@trail.progress}"
+  #     @trail.step
+  #     anim.stop if @trail.complete
+  #   end
+  # end
   
-  def saveData
-    @destfs.merge(@srcfs)
-    @destfs.dump
-  end
+  # def saveData
+  #   @destfs.merge(@srcfs)
+  #   @destfs.dump
+  # end
   
-  def printDebug(text)
-    Hashfs.printDebug(text)
-  end
+  # def printDebug(text)
+  #   Hashfs.printDebug(text)
+  # end
   
 end
