ccfac046830205099929b4e7c32814ff0e1a90a1
lp <lp@spiralix.org>
2009-01-08 09:46:39 -0500

    notice if options are present for network backup when loading

--- shoe_load.rb
+++ shoe_load.rb
@@ -5,11 +5,20 @@ module ShoeHelpers
     @title.replace 'Loading'
     @display.replace 'Please wait while I load your settings'
     @loader = Loader.new
-    if @loader.destination == '!!! /choose/a/path/ !!!'
-      alert("You dont have a destination Folder!\nPlease define one in the option page.")
-      clear; options
+    if @loader.type == 'local'
+      if @loader.destination == '!!! /choose/a/path/ !!!'
+        alert("You dont have a destination Folder!\nPlease define one in the option page.")
+        clear; options
+      else
+        clear; confirm
+      end
     else
-      clear; confirm
+      if @loader.host == 'hostname' || @loader.share == 'sharename' || @loader.password == ''
+        alert("Some Server parameters missing!\nPlease define them in the option page.")
+        clear; options
+      else
+        clear; confirm
+      end
     end
   end
   
