f4c6eac6751a6e0d346db8027d8fba64a101e84e
lp <lp@spiralix.org>
2009-01-30 15:17:45 -0500

    supports server domain

--- shoe_netpath.rb
+++ shoe_netpath.rb
@@ -10,9 +10,14 @@ module ShoeHelpers
         colorContent('share name or IP adress:', red)
         @share = edit_line(@loader.share, :margin => 15)
       end
+			flow do
+        colorContent('Domain:', red)
+        @domain = edit_line(@loader.domain, :margin => 15)
+      end
       flow do
         button('cancel', :margin => 15) { clear; options }
-        button('Done', :margin => 15) {@loader.host = @host.text; @loader.share = @share.text; clear; options}
+        button('Done', :margin => 15) {@loader.host = @host.text; @loader.share = @share.text
+																				@loader.domain = @domain.text; clear; options}
       end
     end
     @title.replace 'Backup Server'
